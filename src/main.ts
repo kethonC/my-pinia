f052bd552f7579c71b33e9bb80b5c3d3f4ebb8ae
GssChen <13560177458@163.com>
2023-04-26 20:53:16 +0800

    demo

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { createApp } from 'vue'
 import { createPinia } from '@/pinia'
 import App from './App.vue'
-import { useCounterStore } from './store/counter'
 
 const app = createApp(App)
-app.use(createPinia())
+const pinia = createPinia()
+// pinia 持久化插件
+pinia.use(({ store }) => {
+  let local = localStorage.getItem(store.$id + 'PINIA_STATE')
+  if (local) {
+    store.$state = JSON.parse(local)
+  }
+
+  store.$subscribe(({ storeId: id }, state) => {
+    localStorage.setItem(id + 'PINIA_STATE', JSON.stringify(state))
+  })
+})
+app.use(pinia)
 app.mount('#app')
+
+// 组件外使用store
+import { useCounterStore } from '@/store/counter'
+
+const countStore = useCounterStore()
+console.log('main.ts', countStore)
